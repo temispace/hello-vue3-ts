7debe45d13e490e8ded8600ebdb9b84ad079618d
dev_siguangming <a18232324036@163.com>
2025-12-25 15:13:12 +0800

    05_toRef和Refs

---
+++ 备份/04_ref 和 reactive使用场景/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <Person/>
+</template>
+<script lang="ts">
+    import Person from './conponents/Person.vue';
+    export default {
+        name: 'App',
+        components:{Person}
+    }
+</script>
+<style>
+    button{
+        margin: 0 5px;
+    }
+</style>
